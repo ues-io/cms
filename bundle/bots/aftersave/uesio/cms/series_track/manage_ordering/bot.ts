f8819ebac3e49b30b85a54d4d5ff683a1508ec51
humandad <55447225+humandad@users.noreply.github.com>
2024-11-18 14:01:14 -0500

    fix prettier on bots

--- bundle/bots/aftersave/uesio/cms/series_track/manage_ordering/bot.ts
+++ bundle/bots/aftersave/uesio/cms/series_track/manage_ordering/bot.ts
@@ -1,171 +1,175 @@
 import { AfterSaveBotApi, WireRecord } from "@uesio/bots"
 
-type SeriesCache = Record<string,Record<string,number>|undefined>
+type SeriesCache = Record<string, Record<string, number> | undefined>
 
 function manage_ordering(bot: AfterSaveBotApi) {
-
-    // Figure out which serieses need processing
-    const seriesesToProcess: SeriesCache = {}
+	// Figure out which serieses need processing
+	const seriesesToProcess: SeriesCache = {}
 
 	bot.inserts.get().forEach((change) => {
+		const currentOrder = change.get("uesio/cms.order") as number
+		const currentSeries = change.get(
+			"uesio/cms.series->uesio/core.id"
+		) as string
+		const currentId = change.get("uesio/core.id") as string
 
-        const currentOrder = change.get("uesio/cms.order") as number
-        const currentSeries = change.get("uesio/cms.series->uesio/core.id") as string
-        const currentId = change.get("uesio/core.id") as string
+		let seriesToProcess = seriesesToProcess[currentSeries]
 
-        let seriesToProcess = seriesesToProcess[currentSeries]
+		if (!seriesToProcess) {
+			seriesesToProcess[currentSeries] = {}
+			seriesToProcess = seriesesToProcess[currentSeries]
+		}
 
-        if (!seriesToProcess) {
-            seriesesToProcess[currentSeries] = {}
-            seriesToProcess = seriesesToProcess[currentSeries]
-        }
-
-        seriesToProcess[currentId] = currentOrder
-        
+		seriesToProcess[currentId] = currentOrder
 	})
 
-    bot.updates.get().forEach((change) => {
-        
-        const currentOrder = change.getOld("uesio/cms.order") as number
-        const currentSeries = change.get("uesio/cms.series->uesio/core.id") as string
-        const currentId = change.get("uesio/core.id") as string
-        
-        const changes = change.getAll()
-        const currentOrderRequest = changes["uesio/cms.order"] as number
-        const hasCurrentValue = currentOrderRequest || currentOrderRequest === 0
-        if (hasCurrentValue && currentOrderRequest !== currentOrder) {
-            
-            let seriesToProcess = seriesesToProcess[currentSeries]
+	bot.updates.get().forEach((change) => {
+		const currentOrder = change.getOld("uesio/cms.order") as number
+		const currentSeries = change.get(
+			"uesio/cms.series->uesio/core.id"
+		) as string
+		const currentId = change.get("uesio/core.id") as string
 
-            if (!seriesToProcess) {
-                seriesesToProcess[currentSeries] = {}
-                seriesToProcess = seriesesToProcess[currentSeries]
-            }
+		const changes = change.getAll()
+		const currentOrderRequest = changes["uesio/cms.order"] as number
+		const hasCurrentValue = currentOrderRequest || currentOrderRequest === 0
+		if (hasCurrentValue && currentOrderRequest !== currentOrder) {
+			let seriesToProcess = seriesesToProcess[currentSeries]
 
-            seriesToProcess[currentId] = currentOrderRequest
-        }
-    })
+			if (!seriesToProcess) {
+				seriesesToProcess[currentSeries] = {}
+				seriesToProcess = seriesesToProcess[currentSeries]
+			}
 
-    bot.deletes.get().forEach((change) => {
+			seriesToProcess[currentId] = currentOrderRequest
+		}
+	})
 
-        const currentSeries = change.getOld("uesio/cms.series->uesio/core.id") as string
-        let seriesToProcess = seriesesToProcess[currentSeries]
+	bot.deletes.get().forEach((change) => {
+		const currentSeries = change.getOld(
+			"uesio/cms.series->uesio/core.id"
+		) as string
+		const seriesToProcess = seriesesToProcess[currentSeries]
 
-        if (!seriesToProcess) {
-            seriesesToProcess[currentSeries] = {}
-        }
+		if (!seriesToProcess) {
+			seriesesToProcess[currentSeries] = {}
+		}
+	})
 
-    })
+	const seriesKeys = Object.keys(seriesesToProcess)
 
-    const seriesKeys = Object.keys(seriesesToProcess)
+	if (!seriesKeys.length) {
+		//bot.log.info("We're done!")
+		return
+	}
 
-    if (!seriesKeys.length) {
-        //bot.log.info("We're done!")
-        return
-    }
+	//bot.log.info("process",seriesesToProcess)
 
-    //bot.log.info("process",seriesesToProcess)
+	// Now do a query for all the tracks we need to process
+	const result = bot.load({
+		collection: "uesio/cms.series_track",
+		fields: [
+			{
+				id: "uesio/cms.order",
+			},
+			{
+				id: "uesio/cms.series",
+				fields: [
+					{
+						id: "uesio/core.id",
+					},
+				],
+			},
+		],
+		conditions: [
+			{
+				field: "uesio/cms.series",
+				operator: "IN",
+				values: seriesKeys,
+			},
+		],
+		order: [
+			{
+				field: "uesio/cms.order",
+				desc: false,
+			},
+		],
+	})
 
-    // Now do a query for all the tracks we need to process
-    const result = bot.load({
-        collection: "uesio/cms.series_track",
-        fields: [{
-            id: "uesio/cms.order"
-        },{
-            id: "uesio/cms.series",
-            fields: [{
-                id: "uesio/core.id",
-            }]
-        }],
-        conditions: [{
-            field: "uesio/cms.series",
-            operator: "IN",
-            values: seriesKeys
-        }],
-        order: [{
-            field: "uesio/cms.order",
-            desc: false,
-        }]
-    })
+	//bot.log.info("result",result)
 
-    //bot.log.info("result",result)
+	// Now put our tracks into buckes by series
+	const currentSerieses: SeriesCache = {}
 
-    // Now put our tracks into buckes by series
-    const currentSerieses: SeriesCache = {}
+	result.forEach((track) => {
+		const currentSeries = track["uesio/cms.series"] as string // This is bad
+		const currentOrder = track["uesio/cms.order"] as number
+		const currentId = track["uesio/core.id"] as string
+		let seriesToProcess = currentSerieses[currentSeries]
 
-    result.forEach((track) => {
+		if (!seriesToProcess) {
+			currentSerieses[currentSeries] = {}
+			seriesToProcess = currentSerieses[currentSeries]
+		}
 
-        const currentSeries = track["uesio/cms.series"] as string // This is bad
-        const currentOrder = track["uesio/cms.order"] as number
-        const currentId = track["uesio/core.id"] as string
-        let seriesToProcess = currentSerieses[currentSeries]
+		seriesToProcess[currentId] = currentOrder
+	})
 
-        if (!seriesToProcess) {
-            currentSerieses[currentSeries] = {}
-            seriesToProcess = currentSerieses[currentSeries]
-        }
+	//bot.log.info("current",currentSerieses)
 
-        seriesToProcess[currentId] = currentOrder
-    })
+	const updates: WireRecord[] = []
 
-    //bot.log.info("current",currentSerieses)
+	// Now loop over our current serieses and put in a correct order
+	Object.keys(currentSerieses).forEach((seriesId) => {
+		let index = 0
+		const currentSeries = currentSerieses[seriesId]
+		if (!currentSeries) {
+			return
+		}
+		const processSeries = seriesesToProcess[seriesId]
+		// Make a map keyed off of request for this series
+		const requestsByIndex: Record<number, string> = {}
+		const requestsById: Record<string, number> = {}
+		// Make a map of successful order requests
+		const successes: Record<string, boolean> = {}
+		if (processSeries) {
+			Object.keys(processSeries).forEach((trackId) => {
+				const requestTrack = processSeries[trackId]
+				if (requestTrack || requestTrack === 0) {
+					requestsByIndex[requestTrack] = trackId
+					requestsById[trackId] = requestTrack
+				}
+			})
+		}
 
-    const updates: WireRecord[] = []
+		//bot.log.info("requests2",requestsByIndex)
 
-    // Now loop over our current serieses and put in a correct order
-    Object.keys(currentSerieses).forEach((seriesId) => {
-        let index = 0
-        const currentSeries = currentSerieses[seriesId]
-        if (!currentSeries) {
-            return
-        }
-        const processSeries = seriesesToProcess[seriesId]
-        // Make a map keyed off of request for this series
-        const requestsByIndex: Record<number,string> = {}
-        const requestsById: Record<string,number> = {}
-        // Make a map of successful order requests
-        const successes: Record<string,boolean> = {}
-        if (processSeries) {
-            Object.keys(processSeries).forEach((trackId) => {
-                const requestTrack = processSeries[trackId]
-                if (requestTrack || requestTrack === 0) {
-                    requestsByIndex[requestTrack] = trackId
-                    requestsById[trackId] = requestTrack
-                }
-            })
-        }
+		Object.keys(currentSeries).forEach((trackId) => {
+			const currentTrack = currentSeries[trackId]
 
-        //bot.log.info("requests2",requestsByIndex)
-        
-        Object.keys(currentSeries).forEach((trackId) => {
-            const currentTrack = currentSeries[trackId]
+			if (requestsByIndex[index]) {
+				// Someone wants this slot
+				updates.push({
+					["uesio/core.id"]: requestsByIndex[index],
+					["uesio/cms.order"]: index,
+				} as unknown as WireRecord)
+				successes[requestsByIndex[index]] = true
+				index++
+			}
+			if (successes[trackId] || requestsById[trackId]) {
+				return
+			}
+			if (currentTrack !== index) {
+				updates.push({
+					["uesio/core.id"]: trackId,
+					["uesio/cms.order"]: index,
+				} as unknown as WireRecord)
+			}
+			index++
+		})
+	})
 
-            if (requestsByIndex[index]) {
-                // Someone wants this slot
-                updates.push({
-                    ["uesio/core.id"]: requestsByIndex[index],
-                    ["uesio/cms.order"]: index
-                } as unknown as WireRecord)
-                successes[requestsByIndex[index]] = true
-                index++
-            }
-            if (successes[trackId] || requestsById[trackId]) {
-                return
-            }
-            if (currentTrack !== index) {
-                updates.push({
-                    ["uesio/core.id"]: trackId,
-                    ["uesio/cms.order"]: index
-                } as unknown as WireRecord)
-            }
-            index++
-            
-        })
-    })
-    
-    //bot.log.info("updates",updates)
-    
-    bot.save("uesio/cms.series_track",updates)
+	//bot.log.info("updates",updates)
 
-   
+	bot.save("uesio/cms.series_track", updates)
 }
